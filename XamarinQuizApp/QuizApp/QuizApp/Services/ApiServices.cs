7a41880147e3225bc76e39e9c92c242e39718dcd
Dragana <dgrujicic16@raf.rs>
2019-05-21 00:15:01 +0200

    Added Register Model

---
+++ XamarinQuizApp/QuizApp/QuizApp/Services/ApiServices.cs
@@ -0,0 +1,45 @@
+﻿using Newtonsoft.Json;
+using QuizApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuizApp.Services
+{
+    public class ApiServices
+    {
+        public async Task<bool> RegisterUserAsync(
+            string email, string password, string confirmPassword)
+        {
+            var client = new HttpClient();
+
+            var model = new RegisterBindingModel
+            {
+                Email = email,
+                Password = password,
+                ConfirmPassword = confirmPassword
+            };
+
+            var json = JsonConvert.SerializeObject(model);
+
+            HttpContent httpContent = new StringContent(json);
+
+            httpContent.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+
+            var response = await client.PostAsync(
+                "http://localhost:56408/api/Account/Register", httpContent);
+
+            if (response.IsSuccessStatusCode)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+
+    }
+}
